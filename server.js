42e3b684351bd2ed24f139ed3e51c9d1b0bf6f35
AlisherMuhammadjonov <ali.java.py@gmail.com>
2025-06-04 16:34:39 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -14,12 +24,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/hello", function (req, res) {
-  res.end("<h1>Hello World by Nolan</h1>");
+app.post("/create-item", (req, res) => {});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
-app.get("/gift", function (req, res) {
-  res.end("<h1>Siz sovgalar sahifasidasiz</h1>");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
